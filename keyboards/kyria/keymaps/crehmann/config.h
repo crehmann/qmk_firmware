fad2867feea77665dbeacb1ca40e2309a9cd0850
Christoph Rehmann <1273098+crehmann@users.noreply.github.com>
2021-03-20 16:05:11 +0100

    Adapted keymap to be similar to miryoku and added support for thinkpad trackpoint

--- keyboards/kyria/keymaps/crehmann/config.h
+++ keyboards/kyria/keymaps/crehmann/config.h
@@ -56,3 +56,25 @@
 
 // Allows media codes to properly register in macros and rotary encoder code
 #define TAP_CODE_DELAY 10
+
+// For PS2 Trackpoint
+#ifdef PS2_USE_INT
+  // clock on pin 3 (d0, int0)
+  #define PS2_CLOCK_PORT PORTD
+  #define PS2_CLOCK_PIN PIND
+  #define PS2_CLOCK_DDR DDRD
+  #define PS2_CLOCK_BIT 0
+  #define PS2_INT_INIT() do { EICRA |= ((1<<ISC01) | (0<<ISC00)); } while (0)
+  #define PS2_INT_ON() do { EIMSK |= (1<<INT0); } while (0)
+  #define PS2_INT_OFF() do { EIMSK &= ~(1<<INT0); } while (0)
+  #define PS2_INT_VECT INT0_vect
+  // data on pin 2 (d1)
+  #define PS2_DATA_PORT PORTD
+  #define PS2_DATA_PIN PIND
+  #define PS2_DATA_DDR DDRD
+  #define PS2_DATA_BIT 1
+  #define PS2_MOUSE_SCROLL_BTN_MASK (1 << PS2_MOUSE_BTN_MIDDLE)
+  #define PS2_MOUSE_SCROLL_BTN_SEND TAPPING_TERM
+  #define PS2_MOUSE_SCROLL_DIVISOR_V 3
+  #define PS2_MOUSE_SCROLL_DIVISOR_H PS2_MOUSE_SCROLL_DIVISOR_V
+#endif
--- keyboards/kyria/keymaps/crehmann/rules.mk
+++ keyboards/kyria/keymaps/crehmann/rules.mk
@@ -2,4 +2,7 @@ OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 LEADER_ENABLE = yes        # Enable the Leader Key feature
+DYNAMIC_MACRO_ENABLE = yes
 MOUSEKEY_ENABLE = yes
+PS2_MOUSE_ENABLE = no
+PS2_USE_INT = no 
