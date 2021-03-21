c9f4d8fed1684109ebb4f6ab508772b8f7e20cff
Christoph Rehmann <1273098+crehmann@users.noreply.github.com>
2021-03-21 20:28:56 +0100

    Added custom transport to transfer layer state between two halves

--- keyboards/kyria/keymaps/crehmann/keymap.c
+++ keyboards/kyria/keymaps/crehmann/keymap.c
@@ -38,11 +38,9 @@ enum layers {
     ADJU
 };
 
-enum custom_keycodes {
-    KC_UE = SAFE_RANGE,
-    KC_AE,
-    KC_OE
-};
+uint16_t record_start_stop_timer;
+bool is_alt_tab_active = false;
+uint16_t alt_tab_timer = 0;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // /*
@@ -57,15 +55,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  * |      | CH_Y |  X   |  C   |  V   |  B   |      |      |  |      |      |  N   |  M   | ,  ; | .  : | -  _ |      |
 //  * |      |      | ALGR |      |      |      |                              |      |      |      | ALGR |      |      |
 //  * `--------------------+------+------+------+------+------|  |------+------+------+------+------+--------------------'
-//  *                      |      | ESC  | SPC  | TAB  | BSPC |  | Lead | ENT  | BSPC | DEL  |      |
+//  *                      | REC1 | ESC  | SPC  | TAB  | BSPC |  | Lead | ENT  | BSPC | DEL  | PLAY1|
 //  *                      |      | MOUR | NAVR | NUMR |      |  |      | FUNL | SYMB | ADJU |      |
 //  *                      `----------------------------------'  `----------------------------------'
 //  */
      [BASE] = LAYOUT(
        _______, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                       CH_Z   , KC_U   , KC_I   , KC_O   , KC_P   , _______,
        _______, GUI_A  , ALT_S  , SFT_D  , CTL_F  , KC_G   ,                                       KC_H   , CTL_J  , SFT_K  , ALT_L  , GUI_ODI, _______,
-       _______, CH_Y   , RALT_X , KC_C   , KC_V   , KC_B   , DM_REC1, DYN_REC_STOP,   DYN_MACRO_PLAY1, _______, KC_N   , KC_M   , KC_COMM, RALT_DT, CH_MINS, _______,
-                                  _______, LT(MOUR, KC_ESC) , LT(NAVR, KC_SPC), LT(NUMR, KC_TAB)  , KC_BSPC,   KC_LEAD, LT(FUNL, KC_ENT) , LT(SYMB, KC_BSPC), LT(ADJU, KC_DEL) , _______
+       _______, CH_Y   , RALT_X , KC_C   , KC_V   , KC_B   , _______, _______,   _______, _______, KC_N   , KC_M   , KC_COMM, RALT_DT, CH_MINS, _______,
+                                  DM_REC1, LT(MOUR, KC_ESC) , LT(NAVR, KC_SPC), LT(NUMR, KC_TAB)  , KC_BSPC,   KC_LEAD, LT(FUNL, KC_ENT) , LT(SYMB, KC_BSPC), LT(ADJU, KC_DEL) , DYN_MACRO_PLAY1
      ),
 
 // /*
@@ -212,31 +210,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // -----------------------------------------------------------------------------------------------------------------------------------
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case KC_UE:
-            if (record->event.pressed) {
-                tap_code16(RALT(KC_Y));
-            }
-            break;
-        case KC_AE:
-            if (record->event.pressed) {
-                tap_code16(RALT(KC_Q));
-            }
-            break;
-        case KC_OE:
-            if (record->event.pressed) {
-                tap_code16(RALT(KC_P));
-            }
-            break;
-    }
-    return true;
-}
-
-
-bool is_alt_tab_active = false;
-uint16_t alt_tab_timer = 0;
-
 LEADER_EXTERNS();
 
 void matrix_scan_user(void) {
@@ -255,13 +228,28 @@ void matrix_scan_user(void) {
             SEND_STRING(SS_LCTRL(SS_LSFT("t")));
         }
         SEQ_ONE_KEY(KC_C) { // Inline Code
-            SEND_STRING("`` "  SS_TAP(X_LEFT));
+            tap_code16(CH_GRV);
+            tap_code16(CH_GRV);
+            tap_code16(KC_LEFT);
         }
         SEQ_TWO_KEYS(KC_C, KC_B) { // Discord code block
-            SEND_STRING("```c" SS_LSFT("\n\n") "``` " SS_TAP(X_UP));
+            tap_code16(CH_GRV);
+            tap_code16(CH_GRV);
+            tap_code16(CH_GRV);
+            tap_code16(CH_GRV);
+            tap_code16(CH_GRV);
+            tap_code16(CH_GRV);
+            tap_code16(KC_LEFT);
+            tap_code16(KC_LEFT);
+            tap_code16(KC_LEFT);
+            tap_code16(KC_ENT);
+            tap_code16(KC_ENT);
+            tap_code16(KC_UP);
         }
         SEQ_TWO_KEYS(KC_L, KC_G) { // Greeting
-            SEND_STRING("Liebe Gr" SS_RALT("p") "sse\nChristoph");
+            SEND_STRING("Liebe Gr");
+            tap_code16(CH_UDIA);
+            SEND_STRING("sse\nChristoph");
         }
     }
 }
@@ -269,20 +257,6 @@ void matrix_scan_user(void) {
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 	return OLED_ROTATION_180;
-}
-
-static void render_kyria_logo(void) {
-    static const char PROGMEM kyria_logo[] = {
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,128,192,224,240,112,120, 56, 60, 28, 30, 14, 14, 14,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7, 14, 14, 14, 30, 28, 60, 56,120,112,240,224,192,128,128,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,192,224,240,124, 62, 31, 15,  7,  3,  1,128,192,224,240,120, 56, 60, 28, 30, 14, 14,  7,  7,135,231,127, 31,255,255, 31,127,231,135,  7,  7, 14, 14, 30, 28, 60, 56,120,240,224,192,128,  1,  3,  7, 15, 31, 62,124,240,224,192,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,240,252,255, 31,  7,  1,  0,  0,192,240,252,254,255,247,243,177,176, 48, 48, 48, 48, 48, 48, 48,120,254,135,  1,  0,  0,255,255,  0,  0,  1,135,254,120, 48, 48, 48, 48, 48, 48, 48,176,177,243,247,255,254,252,240,192,  0,  0,  1,  7, 31,255,252,240,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,255,255,255,  0,  0,  0,  0,  0,254,255,255,  1,  1,  7, 30,120,225,129,131,131,134,134,140,140,152,152,177,183,254,248,224,255,255,224,248,254,183,177,152,152,140,140,134,134,131,131,129,225,120, 30,  7,  1,  1,255,255,254,  0,  0,  0,  0,  0,255,255,255,  0,  0,  0,  0,255,255,  0,  0,192,192, 48, 48,  0,  0,240,240,  0,  0,  0,  0,  0,  0,240,240,  0,  0,240,240,192,192, 48, 48, 48, 48,192,192,  0,  0, 48, 48,243,243,  0,  0,  0,  0,  0,  0, 48, 48, 48, 48, 48, 48,192,192,  0,  0,  0,  0,  0,
-        0,  0,  0,255,255,255,  0,  0,  0,  0,  0,127,255,255,128,128,224,120, 30,135,129,193,193, 97, 97, 49, 49, 25, 25,141,237,127, 31,  7,255,255,  7, 31,127,237,141, 25, 25, 49, 49, 97, 97,193,193,129,135, 30,120,224,128,128,255,255,127,  0,  0,  0,  0,  0,255,255,255,  0,  0,  0,  0, 63, 63,  3,  3, 12, 12, 48, 48,  0,  0,  0,  0, 51, 51, 51, 51, 51, 51, 15, 15,  0,  0, 63, 63,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 48, 48, 63, 63, 48, 48,  0,  0, 12, 12, 51, 51, 51, 51, 51, 51, 63, 63,  0,  0,  0,  0,  0,
-        0,  0,  0,  0, 15, 63,255,248,224,128,  0,  0,  3, 15, 63,127,255,239,207,141, 13, 12, 12, 12, 12, 12, 12, 12, 30,127,225,128,  0,  0,255,255,  0,  0,128,225,127, 30, 12, 12, 12, 12, 12, 12, 12, 13,141,207,239,255,127, 63, 15,  3,  0,  0,128,224,248,255, 63, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  3,  7, 15, 62,124,248,240,224,192,128,  1,  3,  7, 15, 30, 28, 60, 56,120,112,112,224,224,225,231,254,248,255,255,248,254,231,225,224,224,112,112,120, 56, 60, 28, 30, 15,  7,  3,  1,128,192,224,240,248,124, 62, 15,  7,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  3,  7, 15, 14, 30, 28, 60, 56,120,112,112,112,224,224,224,224,224,224,224,224,224,224,224,224,224,224,224,224,112,112,112,120, 56, 60, 28, 30, 14, 15,  7,  3,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-    };
-    oled_write_raw_P(kyria_logo, sizeof(kyria_logo));
 }
 
 static void render_qmk_logo(void) {
@@ -338,7 +312,7 @@ void oled_task_user(void) {
     if (is_keyboard_master()) {
         render_status(); // Renders the current keyboard state (layer, lock, caps, scroll, etc)
     } else {
-        render_kyria_logo();
+        render_status();
     }
 }
 #endif
@@ -347,27 +321,27 @@ void oled_task_user(void) {
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         switch (biton32(layer_state)) {
-            case FUNL: // Underglow hue
+            case ADJU: // Underglow hue
                 if (clockwise) {
-                rgblight_increase_hue();
+                    rgblight_increase_hue();
                 } else {
-                rgblight_decrease_hue();
+                    rgblight_decrease_hue();
                 }
                 break;
-            case MOUR: // Mouse wheel U/D
-                if (clockwise) {
-                    tap_code(KC_WH_U);
-                } else {
-                    tap_code(KC_WH_D);
-                }
-                break;
-            default:
+            case FUNL:
                 // History scrubbing. For Adobe products, hold shift while moving
                 // backward to go forward instead.
                 if (clockwise) {
-                    tap_code16(C(KC_Z));
-                } else {
                     tap_code16(C(KC_Y));
+                } else {
+                    tap_code16(C(KC_Z));
+                }
+                break;
+            default:
+                if (clockwise) {
+                    tap_code(KC_VOLD);
+                } else {
+                    tap_code(KC_VOLU);
                 }
                 break;
         }
--- keyboards/kyria/keymaps/crehmann/rules.mk
+++ keyboards/kyria/keymaps/crehmann/rules.mk
@@ -1,8 +1,33 @@
-OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-LEADER_ENABLE = yes        # Enable the Leader Key feature
+OLED_DRIVER_ENABLE = yes
+ENCODER_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+LEADER_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
 MOUSEKEY_ENABLE = yes
-PS2_MOUSE_ENABLE = no
-PS2_USE_INT = no 
+
+TRACKPOINT = 0
+ifeq ($(TRACKPOINT), 1)
+	PS2_MOUSE_ENABLE = yes
+	PS2_USE_INT = yes 
+	OLED_DRIVER_ENABLE = no
+endif
+
+SPLIT_TRANSPORT = custom
+
+ifeq ($(strip $(SPLIT_TRANSPORT)), custom)
+	OPT_DEFS += -DCUSTOM_TRANSPORT
+	QUANTUM_SRC += transport_custom.c
+	# Functions added via QUANTUM_LIB_SRC are only included in the final binary if they're called.
+	# Unused functions are pruned away, which is why we can add multiple drivers here without bloat.
+	ifeq ($(PLATFORM),AVR)
+		QUANTUM_LIB_SRC += i2c_master.c \
+							i2c_slave.c
+	endif
+
+	SERIAL_DRIVER ?= bitbang
+	ifeq ($(strip $(SERIAL_DRIVER)), bitbang)
+		QUANTUM_LIB_SRC += serial.c
+	else
+		QUANTUM_LIB_SRC += serial_$(strip $(SERIAL_DRIVER)).c
+	endif
+endif
